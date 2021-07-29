a02678b08cc21984c718f6c77d2d26b32753150a
Patrick Weizhi Xu <weizhi.xu@zilliz.com>
2021-07-29 18:45:30 +0800

    fix performance monitor as the number of columns may differ as well (#54)
    * add CPU/Memory/IO monitor
    
    Signed-off-by: Patrick Weizhi Xu <weizhi.xu@zilliz.com>
    
    * fix analyze stats script as time representations differ on different machine
    
    Signed-off-by: Patrick Weizhi Xu <weizhi.xu@zilliz.com>
    
    * the column number may differ
    
    Signed-off-by: Patrick Weizhi Xu <weizhi.xu@zilliz.com>

--- scripts/test_scripts/analyze_stat.py
+++ scripts/test_scripts/analyze_stat.py
@@ -4,6 +4,9 @@ import re
 rst = {}
 header = ""
 
+def split_to_list(l):
+    return re.sub(' +', ' ', l).split()
+
 with open(sys.argv[1]) as f:
     for line in f:
         if line.startswith("Linux"):
@@ -11,15 +14,18 @@ with open(sys.argv[1]) as f:
 
         if line.startswith("#"):
             if not header:
-                header = line
+                header = split_to_list(line)
             continue
-        line = re.sub(' +', ' ', line).split()
+        line = split_to_list(line)
 
         if len(line) == 0:
             continue
 
-        for i in range(-16, -1):
-            if i == -11:
+        i = -2
+        while True:
+            print(header[i], i)
+            if header[i] == "CPU":
+                i -= 1
                 continue
             if not i in rst:
                 rst[i] = line
@@ -27,7 +33,10 @@ with open(sys.argv[1]) as f:
                 if float(line[i]) > float(rst[i][i]):
                     rst[i] = line
 
-header = re.sub(' +', ' ', header).split()
+            if header[i] == "%usr":
+                break
+            
+            i -= 1
 
 for k, v in rst.items():
     if (header[k] == "VSZ" or header[k] == "RSS"):
--- scripts/test_scripts/build.sh
+++ scripts/test_scripts/build.sh
@@ -3,9 +3,11 @@
 pkill -f "build_bigann"
 
 DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"`
-./build_bigann float /mnt/Billion-Scale/Yandex-Text-to-Image/base.1B.fdata /home/zilliz/bigann/test/ 32 500 50 8 IP 128 500 PQRes > build_${DATE_WITH_TIME}.log &
+F=build_${DATE_WITH_TIME}
+./build_bigann float /mnt/Billion-Scale/Yandex-Text-to-Image/base.1B.fdata /home/zilliz/bigann/test/ 32 500 50 8 IP 128 500 PQRes > ${F}.log &
 
 pid=$!
-pidstat -rud -h -t -p $pid 5 > build_${DATE_WITH_TIME}.stat
+pidstat -rud -h -t -p $pid 5 > ${F}.stat
+python3 analyze_stat.py ${F}.stat > ${F}.max.stat
 
 wait $pid
