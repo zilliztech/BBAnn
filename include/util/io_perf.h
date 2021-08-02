1666f7f67d5c180e42565d34c0ebb1cf95d2fd32
Jigao Luo <luojigao@outlook.com>
2021-08-02 14:24:50 +0800

    More on IO metrics.
    Signed-off-by: Jigao Luo <luojigao@outlook.com>

---
+++ include/util/io_perf.h
@@ -0,0 +1,207 @@
+#pragma once
+// ----------------------------------------------------------------------------------------------------
+#include <unistd.h>
+#include <cassert>
+#include <cstdlib>
+#include <fstream>
+#include <array>
+// ----------------------------------------------------------------------------------------------------
+/**
+ * A class parsing a line of file /proc/$PID/io
+ * Might need a super user permission to access: /proc/$PID/io
+ */
+class PID_IO {
+ private:
+    pid_t pid;
+    /// rchar + wchar := might in page cache
+    size_t rchar;
+    size_t wchar;
+
+    /// syscr + syscw := real IO
+    size_t syscr;
+    size_t syscw;
+
+    size_t read_bytes;
+    size_t write_bytes;
+    size_t cancelled_write_bytes;
+
+
+ public:
+    PID_IO() {
+        pid = getpid();
+        const auto pid_str = std::to_string(pid);
+        const auto filename = "/proc/" + pid_str + "/io";
+        std::ifstream file(filename);
+        assert(!file.bad());
+        std::string line;
+        while (std::getline(file, line)) {
+            if (line.substr(0, 7) == "rchar: ") {
+                const auto number_str = line.substr(7);
+                rchar = std::stoull(number_str);
+            } else if (line.substr(0, 7) == "wchar: ") {
+                const auto number_str = line.substr(7);
+                wchar = std::stoull(number_str);
+            } else if (line.substr(0, 7) == "syscr: ") {
+                const auto number_str = line.substr(7);
+                syscr = std::stoull(number_str);
+            } else if (line.substr(0, 7) == "syscw: ") {
+                const auto number_str = line.substr(7);
+                syscw = std::stoull(number_str);
+            } else if (line.substr(0, 12) == "read_bytes: ") {
+                const auto number_str = line.substr(12);
+                read_bytes = std::stoull(number_str);
+            } else if (line.substr(0, 13) == "write_bytes: ") {
+                const auto number_str = line.substr(13);
+                write_bytes = std::stoull(number_str);
+            } else if (line.substr(0, 23) == "cancelled_write_bytes: ") {
+                const auto number_str = line.substr(23);
+                cancelled_write_bytes = std::stoull(number_str);
+            }
+        }
+    }
+
+    pid_t getPid() const { return pid; }
+
+    size_t getRchar() const { return rchar; }
+
+    size_t getWchar() const { return wchar; }
+
+    size_t getSyscr() const { return syscr; }
+
+    size_t getSyscw() const { return syscw; }
+
+    size_t getReadBytes() const { return read_bytes; }
+
+    size_t getWriteBytes() const { return write_bytes; }
+
+    size_t getCancelledWriteBytes() const { return cancelled_write_bytes; }
+};
+// ----------------------------------------------------------------------------------------------------
+class PID_IO_Counter {
+private:
+    PID_IO pre_iamge;
+
+public:
+    PID_IO_Counter() : pre_iamge(PID_IO()) {}
+
+    ~PID_IO_Counter() {
+        const PID_IO post_iamge = PID_IO();
+        std::cout << "# rchar: " << post_iamge.getRchar() - pre_iamge.getRchar() << std::endl;
+        std::cout << "# wchar: " << post_iamge.getWchar() - pre_iamge.getWchar() << std::endl;
+        std::cout << "# syscr: " << post_iamge.getSyscr() - pre_iamge.getSyscr() << std::endl;
+        std::cout << "# syscw: " << post_iamge.getSyscw() - pre_iamge.getSyscw() << std::endl;
+        std::cout << "# read_bytes: " << post_iamge.getReadBytes() - pre_iamge.getReadBytes() << std::endl;
+        std::cout << "# write_bytes: " << post_iamge.getWriteBytes() - pre_iamge.getWriteBytes() << std::endl;
+        std::cout << "# cancelled_write_bytes: " << post_iamge.getCancelledWriteBytes() - pre_iamge.getCancelledWriteBytes() << std::endl;
+    }
+};
+// ----------------------------------------------------------------------------------------------------
+/**
+ * A class parsing a line of file /proc/diskstats
+ * Need a super user permission to access: /proc/diskstats
+ */
+class DiskStat {
+ private:
+    std::string device_name;
+    std::array<uint64_t, 18> fields;  // fields[0] is left unused.
+    // Field  1 -- # of reads completed
+    // Field  2 -- # of reads merged
+    // Field  3 -- # of sectors read
+    // Field  4 -- # of milliseconds spent reading
+
+    // Field  5 -- # of writes completed
+    // Field  6 -- # of writes merged
+    // Field  7 -- # of sectors written
+    // Field  8 -- # of milliseconds spent writing
+
+    // Field  9 -- # of I/Os currently in progress
+    // Field 10 -- # of milliseconds spent doing I/Os
+    // Field 11 -- weighted # of milliseconds spent doing I/Os
+
+    // Field 12 -- # of discards completed
+    // Field 13 -- # of discards merged
+    // Field 14 -- # of sectors discarded
+    // Field 15 -- # of milliseconds spent discarding
+
+    // Field 16 -- # of flush requests completed
+    // Field 17 -- # of milliseconds spent flushing
+
+    /**
+     * Private helper function. The only way to construct a DiskStat.
+     * @param info a line of file /proc/diskstats
+     */
+    DiskStat(std::string device_name, std::string info) {
+        const auto first_space = info.find(' ');
+        auto main_str = info.substr(first_space + 1);
+        assert(main_str.front() != ' ');
+        this->device_name = info.substr(0, first_space);
+        assert(device_name == this->device_name);
+        // You should have a Kernel  5.5+ to reach all 17 fields.
+        for (auto i = 1; i < 18; ++i) {
+            const auto next_space = main_str.find(' ');
+            fields[i] = std::stoull(main_str.substr(0, next_space));
+            main_str = main_str.substr(next_space + 1);
+        }
+    }
+
+    DiskStat() = delete;
+
+ public:
+    /**
+     * Read /proc/diskstats to get disk's info.
+     * Need a super user permission to access: /proc/diskstats
+     * @return
+     */
+    static DiskStat read_IO_DiskStat(std::string device_name = "nvme0n1") {
+        const auto filename = "/proc/diskstats";
+        std::ifstream file(filename);
+        assert(!file.bad());
+        std::string line;
+        while (std::getline(file, line)) {
+            const auto n = line.find(device_name);
+            if (n != std::string::npos) {
+                const auto io_str = line.substr(n);
+                return DiskStat(device_name, io_str);
+            }
+        }
+        __builtin_unreachable(); // Wrong device name.
+    }
+
+    /**
+     * @param field_index
+     * @return get a field
+     */
+    uint64_t get_nth_field(int field_index) const {
+        assert(field_index != 0);
+        return fields.at(field_index);
+    }
+
+    /**
+     * @param field_index
+     * @return get a field
+     */
+    uint64_t operator[](int field_index) const {
+        assert(field_index != 0);
+        return fields.at(field_index);
+    }
+};
+// ----------------------------------------------------------------------------------------------------
+class DiskStat_Read_Counter {
+ private:
+    DiskStat pre_iamge;
+
+ public:
+    DiskStat_Read_Counter() : pre_iamge(DiskStat::read_IO_DiskStat()) {}
+
+    ~DiskStat_Read_Counter() {
+        DiskStat post_iamge = DiskStat::read_IO_DiskStat();
+        std::cout << "# read completed: " << post_iamge[1] - pre_iamge[1] << std::endl;
+        std::cout << "# read merged: " << post_iamge[2] - pre_iamge[2] << std::endl;
+        std::cout << "# sectors read: " << post_iamge[3] - pre_iamge[3] << std::endl;
+        std::cout << "# milliseconds spent reading: " << post_iamge[4] - pre_iamge[4] << std::endl;
+        std::cout << "# of milliseconds spent doing I/Os: " << post_iamge[10] - pre_iamge[10] << std::endl;
+        std::cout << "weighted # of milliseconds spent doing I/Os: " << post_iamge[11] - pre_iamge[11] << std::endl;
+
+    }
+};
+// ----------------------------------------------------------------------------------------------------
---
+++ test/helloworld_io_perf.cpp
@@ -0,0 +1,86 @@
+// ----------------------------------------------------------------------------------------------------
+#include <iostream>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+// ----------------------------------------------------------------------------------------------------
+#include "util/io_perf.h"
+// ----------------------------------------------------------------------------------------------------
+void write_mmap_sample_data() {
+    int fd;
+    char ch;
+    struct stat textfilestat;
+    fd = open("MMAP_DATA.txt", O_CREAT|O_TRUNC|O_WRONLY, 0777);
+    if (fd == -1) {
+        perror("File open error ");
+        return;
+    }
+    // Write A to Z
+    ch = 'A';
+    for (auto i = 0; i < 10'000'000; ++i) {
+        write(fd, &ch, sizeof(ch));
+    }
+    close(fd);
+    return;
+}
+// ----------------------------------------------------------------------------------------------------
+int IO_function() {
+    struct stat mmapstat;
+    char *data;
+    int minbyteindex;
+    int maxbyteindex;
+    int offset;
+    int fd;
+    int unmapstatus;
+    write_mmap_sample_data();
+    if (stat("MMAP_DATA.txt", &mmapstat) == -1) {
+        perror("stat failure");
+        return 1;
+    }
+
+    if ((fd = open("MMAP_DATA.txt", O_RDONLY)) == -1) {
+        perror("open failure");
+        return 1;
+    }
+    data = static_cast<char*>(mmap((caddr_t)0, mmapstat.st_size, PROT_READ, MAP_SHARED, fd, 0));
+
+    if (data == (caddr_t)(-1)) {
+        perror("mmap failure");
+        return 1;
+    }
+    minbyteindex = 0;
+    maxbyteindex = mmapstat.st_size - 1;
+
+    unmapstatus = munmap(data, mmapstat.st_size);
+
+    if (unmapstatus == -1) {
+        perror("munmap failure");
+        return 1;
+    }
+    close(fd);
+    system("rm -f MMAP_DATA.txt");
+}
+// ----------------------------------------------------------------------------------------------------
+int main() {
+    std::cout << "------------------------------------------------" << std::endl;
+    {
+#if IOPERF
+        PID_IO_Counter s;
+#endif
+        auto result = IO_function();
+    }
+    std::cout << "------------------------------------------------" << std::endl;
+    {
+#if IOPERF
+        DiskStat_Read_Counter s;
+#endif
+        auto result = IO_function();
+    }
+    return 0;
+}
+// ----------------------------------------------------------------------------------------------------
+
