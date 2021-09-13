bfc7fcab1bafacc62069d6004af5018ad54d0c15
Jigao Luo <luojigao@outlook.com>
2021-09-13 16:38:06 +0800

    fix gt reader.
    Signed-off-by: Jigao Luo <luojigao@outlook.com>

--- playground/gt_reader.cpp
+++ playground/gt_reader.cpp
@@ -9,7 +9,6 @@
 //---------------------------------------------------------------------------
 namespace {
 //---------------------------------------------------------------------------
-template <typename T>
 void read_ground_truth(const std::string& input_path) {
     // The ground truth binary files for k-NN search consist of the following information:
     //   num_queries(uint32_t) K-NN(uint32) followed by
@@ -40,33 +39,24 @@ void read_ground_truth(const std::string& input_path) {
 
     std::cout << std::endl;
 
-    T min_distance = std::numeric_limits<T>::max();
-    T max_distance = std::numeric_limits<T>::min();
+    float min_distance = std::numeric_limits<T>::max();
+    float max_distance = std::numeric_limits<T>::min();
     for (size_t i = 0; i < num_queries; ++i) {
         std::cout << i << "-th query: ";
-        T distance;
+        float distance;
         for (size_t j = 0; j < knn; ++j) {
             input.read(reinterpret_cast<char*>(&distance), sizeof(distance));
-            min_distance = std::min<T>(min_distance, distance);
-            max_distance = std::max<T>(max_distance, distance);
-            if constexpr (std::is_same_v<T, float>) {
-                std::cout << distance << " ";
-            } else {
-                std::cout << static_cast<int32_t>(distance) << " ";
-            }
+            min_distance = std::min(min_distance, distance);
+            max_distance = std::max(max_distance, distance);
+            std::cout << distance << " ";
         }
         std::cout << std::endl;
     }
 
     std::cout << "min nn id: " << min_nn_id << std::endl;
     std::cout << "max nn id: " << max_nn_id << std::endl;
-    if constexpr (std::is_same_v<T, float>) {
-        std::cout << "min distance: " << min_distance << std::endl;
-        std::cout << "min distance: " << max_distance << std::endl;
-    } else {
-        std::cout << "min distance: " << static_cast<int32_t>(min_distance) << std::endl;
-        std::cout << "min distance: " << static_cast<int32_t>(max_distance) << std::endl;
-    }
+    std::cout << "min distance: " << min_distance << std::endl;
+    std::cout << "min distance: " << max_distance << std::endl;
 
     input.close();
 }
@@ -74,32 +64,14 @@ void read_ground_truth(const std::string& input_path) {
 } // namespace
 //---------------------------------------------------------------------------
 int main() {
-    // TODO: NO Range Search Dataset
+    // TODO: No Range Search Dataset as Input
     std::cout << "Please type in the input file input_path into the std::cin:" << std::endl;
     std::cout << "Example: \"/home/jigao/Desktop/GT_10M_v2/GT_10M/bigann-10M\"" << std::endl;
     std::string input_path;
     std::cin >> input_path;
     std::cout << "The input input_path is: " << input_path << std::endl;
 
-    std::cout << "Please type in the data type: uint8, int8 or float32: " << std::endl;
-    std::cout << "Example: \"uint8\"" << std::endl;
-    std::string data_type;
-    std::cin >> data_type;
-    if (data_type != "uint8" && data_type != "int8" && data_type != "float32") {
-        std::cerr << "Wrong data type: " << data_type << std::endl;
-        return 1;
-    }
-    std::cout << "The data type is: " << data_type << std::endl;
-
-    read_ground_truth<float>(input_path);
-
-//    if (data_type == "uint8") {
-//        read_ground_truth<uint8_t>(input_path);
-//    } else if (data_type == "int8") {
-//        read_ground_truth<int8_t>(input_path);
-//    } else if (data_type == "float32") {
-//        read_ground_truth<float>(input_path);
-//    }
+    read_ground_truth(input_path);
     return 0;
 }
 //---------------------------------------------------------------------------
