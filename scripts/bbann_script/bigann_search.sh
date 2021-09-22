062fcf5372a1cd8c221ddde224ec8809b72918b9
Jigao Luo <luojigao@outlook.com>
2021-09-22 10:29:17 +0800

    add search script for bigann.
    Signed-off-by: Jigao Luo <luojigao@outlook.com>

--- scripts/bbann_script/bigann_search.sh
+++ scripts/bbann_script/bigann_search.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
+DATA_TYPE=uint8
+K=10
+METRIC_TYPE=L2
 # ================================================================================
 # ========================Begin Of Parameters=====================================
 # ================================================================================
-DATA_TYPE=uint8
-#DATA_TYPE=int8
-#DATA_TYPE=float
-INDEX_PATH=/data/GRIP_Indexes/BIGANN-32-500-32-8-128-500-PQR/
-QUERY_FILE=/data/diskann-T2-baseline-indices/bigann-1B/query.public.10K.u8bin
+# DATA_TYPE
+INDEX_PATH=/data/10M_test/indexes
+QUERY_FILE=/data/10M_test/datasets/query.public.10K.u8bin
 RESULT_OUTPUT=/data/answers/a.answer
-TRUTH_SET_FILE=/data/diskann-T2-baseline-indices/bigann-1B/bigann-1B-gt
+TRUTH_SET_FILE=/data/10M_test/datasets/bigann-10M-gt
 NPROBE=50
 REFINE_NPROBE=250
 # K
 K1=128
-METRIC_TYPE=L2
-#METRIC_TYPE=IP
+# METRIC_TYPE
 PAGE_PER_BLOCK=3
 
-LOG_FILE=/data/0918bbann_search.log
+LOG_FILE=/data/0922bbann_search.log
 # ================================================================================
 # ===========================End Of Parameters====================================
 # ================================================================================
@@ -26,21 +26,13 @@ LOG_FILE=/data/0918bbann_search.log
 echo "Please run this script with root permission."
 echo "Copy this file as well as analyze_stat.py to release/ folder, where project is compiled."
 
-if [ "$DATA_TYPE" = uint8 ]; then
-  echo "Data Type: uint8 for BIGANN, Facebook SimSearchNet++"
-elif [ "$DATA_TYPE" = int8 ]; then
-  echo "Data Type: int8 for Microsoft SPACEV"
-elif [ "$DATA_TYPE" = float ]; then
-  echo "float: float for Microsoft Turing-ANNS, Yandex DEEP, Yandex Text-to-Image"
-fi
-
+echo "Data Type: uint8 for BIGANN"
 echo "Index Folder: " $INDEX_PATH
 echo "QUERY_FILE: " $QUERY_FILE
 echo "Result Answer Path: " $RESULT_OUTPUT
 echo "TRUTH_SET_FILE: " $TRUTH_SET_FILE
 echo "nprobe: " $NPROBE
 echo "refine nprobe: " $REFINE_NPROBE
-K=10
 echo "Top K: " $K
 echo "K1 (cluster number): " $K1
 echo "Metric Type: " $METRIC_TYPE
