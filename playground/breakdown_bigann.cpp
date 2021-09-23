bb92302b32619c51db4ea77a5e84ede395d2aba6
Jigao Luo <luojigao@outlook.com>
2021-09-23 08:28:12 +0800

    add distance without simd.
    Signed-off-by: Jigao Luo <luojigao@outlook.com>

--- playground/breakdown_bigann.cpp
+++ playground/breakdown_bigann.cpp
@@ -61,7 +61,7 @@ static void InMemory(benchmark::State& st) {
     if (input.read(reinterpret_cast<char*>(buffer.data()), size)) {
         for (auto _ : st) {
             for (int i = 0; i < slice_size; ++i){
-                const int len = L2sqr<uint8_t, uint8_t, int>(buffer.data(), buffer.data() + num_dimensions * i, num_dimensions);
+                const int len = L2sqr_PLAIN(buffer.data(), buffer.data() + num_dimensions * i, num_dimensions);
                 sum += len;
             }
         }
@@ -73,6 +73,59 @@ static void InMemory(benchmark::State& st) {
 }
 // ---------------------------------------------------------------------------
 static void InMemory_MT(benchmark::State& st) {
+    std::ifstream input(base_file_path, std::ios::binary);
+    uint32_t num_points;
+    uint32_t num_dimensions;
+    assert(input.is_open());
+    input.read(reinterpret_cast<char*>(&num_points), sizeof(num_points));
+    input.read(reinterpret_cast<char*>(&num_dimensions), sizeof(num_dimensions));
+    uint32_t slice_size = st.range(0);
+    assert(num_points >= slice_size);
+    const auto size = num_dimensions * slice_size * sizeof(uint8_t);
+    std::vector<uint8_t> buffer(size);
+    uint64_t sum = 0;
+    if (input.read(reinterpret_cast<char*>(buffer.data()), size)) {
+        for (auto _ : st) {
+#pragma omp parallel for
+            for (int i = 0; i < slice_size; ++i){
+                const int len = L2sqr_PLAIN(buffer.data(), buffer.data() + num_dimensions * i, num_dimensions);
+                sum += len;
+            }
+        }
+    }
+//    st.counters["num_points"] = num_points;
+//    st.counters["num_dimensions"] = num_dimensions;
+    st.counters["slice_size"] = slice_size;
+    st.counters["SUM of L2 SQR"] = sum;
+}
+// ---------------------------------------------------------------------------
+static void InMemory_SIMD(benchmark::State& st) {
+    std::ifstream input(base_file_path, std::ios::binary);
+    uint32_t num_points;
+    uint32_t num_dimensions;
+    assert(input.is_open());
+    input.read(reinterpret_cast<char*>(&num_points), sizeof(num_points));
+    input.read(reinterpret_cast<char*>(&num_dimensions), sizeof(num_dimensions));
+    uint32_t slice_size = st.range(0);
+    assert(num_points >= slice_size);
+    const auto size = num_dimensions * slice_size * sizeof(uint8_t);
+    std::vector<uint8_t> buffer(size);
+    uint64_t sum = 0;
+    if (input.read(reinterpret_cast<char*>(buffer.data()), size)) {
+        for (auto _ : st) {
+            for (int i = 0; i < slice_size; ++i){
+                const int len = L2sqr<uint8_t, uint8_t, int>(buffer.data(), buffer.data() + num_dimensions * i, num_dimensions);
+                sum += len;
+            }
+        }
+    }
+//    st.counters["num_points"] = num_points;
+//    st.counters["num_dimensions"] = num_dimensions;
+    st.counters["slice_size"] = slice_size;
+    st.counters["SUM of L2 SQR"] = sum;
+}
+// ---------------------------------------------------------------------------
+static void InMemory_SIMD_MT(benchmark::State& st) {
     std::ifstream input(base_file_path, std::ios::binary);
     uint32_t num_points;
     uint32_t num_dimensions;
@@ -109,9 +162,9 @@ static void CPP_Fread(benchmark::State& st) {
     uint32_t slice_size = st.range(0);
     assert(num_points >= slice_size);
     const auto size = num_dimensions * slice_size * sizeof(uint8_t);
-    std::vector<uint8_t> buffer(size);
     for (auto _ : st) {
         st.PauseTiming(); clean_page_cache(); st.ResumeTiming();
+        std::vector<uint8_t> buffer(size);
         input.read(reinterpret_cast<char*>(buffer.data()), size);
     }
 //    st.counters["num_points"] = num_points;
@@ -249,6 +302,8 @@ static void SequentialRead_HT(benchmark::State& st) {
 // ---------------------------------------------------------------------------
 BENCHMARK(InMemory)->Iterations(10)->Unit(benchmark::kMillisecond)->Arg(1'000'000)->Arg(10'000'000);
 BENCHMARK(InMemory_MT)->Iterations(10)->Unit(benchmark::kMillisecond)->Arg(1'000'000)->Arg(10'000'000);
+BENCHMARK(InMemory_SIMD)->Iterations(10)->Unit(benchmark::kMillisecond)->Arg(1'000'000)->Arg(10'000'000);
+BENCHMARK(InMemory_SIMD_MT)->Iterations(10)->Unit(benchmark::kMillisecond)->Arg(1'000'000)->Arg(10'000'000);
 BENCHMARK(CPP_Fread)->Iterations(10)->Unit(benchmark::kMillisecond)->Arg(1'000'000)->Arg(10'000'000);
 BENCHMARK(SequentialRead)->Iterations(10)->Unit(benchmark::kMillisecond)->Arg(1'000'000)->Arg(10'000'000);
 BENCHMARK(SequentialRead_HT)->Iterations(10)->Unit(benchmark::kMillisecond)->Arg(1'000'000)->Arg(10'000'000);
--- playground/util.h
+++ playground/util.h
@@ -9,3 +9,22 @@ void clean_page_cache() {
     if (1 != write(fd, "3", 1)) std::cout << "Fail to write drop cache file." << std::endl;
     close(fd);
 }
+
+int L2sqr_PLAIN(uint8_t *a, uint8_t *b, size_t n) {
+    size_t i = 0;
+    size_t dis = 0, dif;
+    switch(n & 7) {
+        default:
+            while (n > 7) {
+                n -= 8; dif=(uint8_t)a[i]-(uint8_t)b[i]; dis+=dif*dif; i++;
+                case 7: dif=(uint8_t)a[i]-(uint8_t)b[i]; dis+=dif*dif; i++;
+                case 6: dif=(uint8_t)a[i]-(uint8_t)b[i]; dis+=dif*dif; i++;
+                case 5: dif=(uint8_t)a[i]-(uint8_t)b[i]; dis+=dif*dif; i++;
+                case 4: dif=(uint8_t)a[i]-(uint8_t)b[i]; dis+=dif*dif; i++;
+                case 3: dif=(uint8_t)a[i]-(uint8_t)b[i]; dis+=dif*dif; i++;
+                case 2: dif=(uint8_t)a[i]-(uint8_t)b[i]; dis+=dif*dif; i++;
+                case 1: dif=(uint8_t)a[i]-(uint8_t)b[i]; dis+=dif*dif; i++;
+            }
+    }
+    return dis;
+}
