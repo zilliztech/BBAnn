0a147c7413cd0fac19e31de4fe7285faf6f21d56
Cat1andCat2 <@@>
2021-09-27 14:42:04 +0000

    force rebuild c++ for all pybind

--- python/instruction.sh
+++ python/instruction.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # To build the pybind11 integration, run the following:
 
+pushd ../build
+cmake ..
+make -j
+popd
+
 sudo python3 setup.py install
 
 python3 tests/bbann_build_test.py --type float --data_path /data/random-xs/random10000/data_10000_20 --save_path /tmp/dat/ 
