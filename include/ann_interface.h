7c64fd907bf300ab5993d3282de6524569ab20fb
Cat1andCat2 <@@>
2021-09-30 04:41:31 +0000

    separet impl and decl

---
+++ include/ann_interface.h
@@ -0,0 +1,43 @@
+// defines interface for AnnIndex
+#pragma once
+#include <stdint.h>
+#include <string>
+
+template <typename T> struct TypeWrapper;
+template <> struct TypeWrapper<float> { using distanceT = float; };
+template <> struct TypeWrapper<uint8_t> { using distanceT = uint32_t; };
+template <> struct TypeWrapper<int8_t> { using distanceT = int32_t; };
+
+template <class TClass, typename paraT> class BuildIndexFactory {
+public:
+  static void BuildIndex(const paraT para) { TClass::BuildIndexImpl(para); }
+};
+// Interface for Ann index which the base data type of the vector space is
+// dataT, and the index should be accessed via parameters/settings packed in
+// paraT.
+//    dataT should be one of [uint8_t, int8_t, float(32-bit)].
+//    see BBAnnParameters for example paraT.
+template <typename dataT, typename paraT> class AnnIndexInterface {
+  using parameterType = paraT;
+  using dataType = dataT;
+  using distanceT = typename TypeWrapper<dataT>::distanceT;
+
+public:
+  virtual ~AnnIndexInterface() = default;
+
+  // Load the in-mem part of the index into memory.
+  // Returns true if load success.
+  virtual bool LoadIndex(std::string &indexPathPrefix) = 0;
+
+  // To construct a index with parameters and settings given in *para*,
+  // users shall call <>.BuildIndex().
+  // The class that implements the interface must contain this method:
+  // static void BuildIndexImpl(const paraT para);
+
+  // Conduct a top-k Ann search for items in *pquery*, which is an arry of dataT
+  // type, converted from numpy array of dim*numQuery items .
+  virtual void BatchSearchCpp(const dataT *pquery, uint64_t dim,
+                              uint64_t numQuery, uint64_t knn, const paraT para,
+                              uint32_t *answer_ids,
+                              distanceT *answer_dists) = 0;
+};
---
+++ src/lib/bbannlib.cpp
@@ -0,0 +1,93 @@
+#include "lib/bbannlib.h"
+#include "ann_interface.h"
+#include "util/constants.h"
+#include "util/defines.h"
+#include <stdint.h>
+#include <string>
+
+std::string TestLib::Get() { return "hi"; }
+
+template <typename dataT, typename paraT>
+bool BBAnnIndex<dataT, paraT>::LoadIndex(std::string &indexPathPrefix) {
+  std::cout << "Loading: " << indexPathPrefix;
+
+  std::string hnsw_index_file = indexPathPrefix + HNSW + INDEX + BIN;
+  std::string bucket_centroids_file =
+      indexPathPrefix + BUCKET + CENTROIDS + BIN;
+
+  uint32_t bucket_num, dim;
+  get_bin_metadata(bucket_centroids_file, bucket_num, dim);
+
+  hnswlib::SpaceInterface<float> *space = nullptr;
+  if (MetricType::L2 == metric_) {
+    space = new hnswlib::L2Space(dim);
+  } else if (MetricType::IP == metric_) {
+    space = new hnswlib::InnerProductSpace(dim);
+  }
+  // load hnsw
+  index_hnsw_ =
+      std::make_shared<hnswlib::HierarchicalNSW<float>>(space, hnsw_index_file);
+  indexPrefix_ = indexPathPrefix;
+  return true;
+}
+
+template <typename dataT, typename paraT>
+void BBAnnIndex<dataT, paraT>::BatchSearchCpp(const dataT *pquery, uint64_t dim,
+                                              uint64_t numQuery, uint64_t knn,
+                                              const paraT para,
+                                              uint32_t *answer_ids,
+                                              distanceT *answer_dists) {
+  std::cout << "Query: " << std::endl;
+  switch (para.metric) {
+  case MetricType::L2: {
+    Computer<dataT, dataT, distanceT> dis_computer =
+        L2sqr<const dataT, const dataT, distanceT>;
+    search_bbann_queryonly<dataT, distanceT, CMax<distanceT, uint32_t>>(
+        indexPrefix_, para.nProbe, para.hnswefC, knn, index_hnsw_, para.K1,
+        para.blockSize, dis_computer, pquery, answer_ids, answer_dists,
+        numQuery, dim);
+    break;
+  }
+  default:
+    std::cerr << "not supported" << std::endl;
+  }
+}
+
+template <typename dataT, typename paraT>
+void BBAnnIndex<dataT, paraT>::BuildIndexImpl(const paraT para) {
+  std::cout << "Build start " << std::endl;
+  using distanceT = typename TypeWrapper<dataT>::distanceT;
+  switch (para.metric) {
+  case MetricType::L2: {
+    std::cout << "Build With L2" << std::endl;
+    std::cout << "dataT" << typeid(dataT).name() << std::endl;
+    std::cout << "distanceT" << typeid(distanceT).name() << std::endl;
+    build_bbann<dataT, distanceT, CMax<distanceT, uint32_t>>(
+        para.dataFilePath, para.indexPrefixPath, para.hnswM, para.hnswefC,
+        para.metric, para.K1, para.blockSize);
+    return;
+  }
+  case MetricType::IP: {
+    build_bbann<dataT, distanceT, CMin<distanceT, uint32_t>>(
+        para.dataFilePath, para.indexPrefixPath, para.hnswM, para.hnswefC,
+        para.metric, para.K1, para.blockSize);
+    return;
+  }
+  default:
+    std::cerr << "not supported" << std::endl;
+  }
+}
+
+#define BBANNLIB_DECL(dataT, paraT)                                            \
+  template bool BBAnnIndex<dataT, paraT>::LoadIndex(                           \
+      std::string &indexPathPrefix);                                           \
+  template void BBAnnIndex<dataT, paraT>::BatchSearchCpp(                      \
+      const dataT *pquery, uint64_t dim, uint64_t numQuery, uint64_t knn,      \
+      const paraT para, uint32_t *answer_ids, distanceT *answer_dists);        \
+  template void BBAnnIndex<dataT, paraT>::BuildIndexImpl(const paraT para);
+
+BBANNLIB_DECL(float, BBAnnParameters);
+BBANNLIB_DECL(uint8_t, BBAnnParameters);
+BBANNLIB_DECL(int8_t, BBAnnParameters);
+
+#undef BBANNLIB_DECL
