55398f8481ec469712d28322c778977b12e7f905
cqy123456 <39671710+cqy123456@users.noreply.github.com>
2021-10-04 17:53:19 +0800

    sample clustering (#140)
    Signed-off-by: cqy123456 <qianya.cheng@zilliz.com>

--- include/util/constants.h
+++ include/util/constants.h
@@ -17,6 +17,8 @@ constexpr static uint64_t GIGABYTE = 1024 * 1024 * 1024;
 constexpr static float K1_SAMPLE_RATE = 0.01;
 // sample rate of the pq train set
 constexpr static float PQ_SAMPLE_RATE = 0.01;
+// limit the training size of the k2 clustering
+constexpr static int K2_MAX_POINTS_PER_CENTROID = 256;
 // the threshold of the second round k-means, if the size of cluster is larger than this threshold, than do ((cluster size)/threshold)-means
 constexpr static int SPLIT_THRESHOLD = 500;
 // the max cluster number in hierarchical_cluster
