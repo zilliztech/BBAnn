0548625d570e5a3739f753c40de4c191a7190fb5
Cat1andCat2 <@@>
2021-10-10 01:30:45 +0000

    update script to run run.py

--- python/run_framework.sh
+++ python/run_framework.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # To build the pybind11 integration, run the following:
+# need to create symbolic link ../benchmark/data at certain SSD.
+# then,
+# python3 create_dataset.py --datset random-xs
+
 set -e
 pushd ../benchmark
-python3 run.py --definitions ../python/bbann-algo.yaml --nodocker --dataset random-xs --algorithm bbann 
-python3 plot.py --definitions ../python/bbann-algo.yaml --dataset random-xs
+python3 run.py --definitions ../python/bbann-algo.yaml --nodocker --dataset random-xs --algorithm bbann --force --rebuild
+python3 plot.py --definitions ../python/bbann-algo.yaml --dataset random-xs 
 popd
