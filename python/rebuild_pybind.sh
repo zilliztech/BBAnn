d42607632ce2625ec181b6ac5f6a2de8db5cca84
Cat1andCat2 <@@>
2021-10-12 07:11:52 +0000

    update build script

--- python/rebuild_pybind.sh
+++ python/rebuild_pybind.sh
@@ -4,18 +4,22 @@ set -e
 mkdir -p ../build
 pushd ../build
 cmake ..
+cd src
 make clean
 make -j
 popd
 
+sudo rm -rf build/
+sudo rm -rf /tmp/dat
+sudo mkdir /tmp/dat
 sudo python3 setup.py install
 
-python3 tests/bbann_build_test.py \
+sudo python3 tests/bbann_build_test.py \
   --type float \
   --data_path /data/random-xs/random10000/data_10000_20 \
   --save_path /tmp/dat/ 
 
-python3 tests/bbann_query_test.py \
+sudo python3 tests/bbann_query_test.py \
    --type float --index_path_prefix /tmp/dat/ \
    --query_path /data/random-xs/random10000/queries_1000_20
 echo Succ!
