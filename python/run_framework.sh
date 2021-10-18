9389cba8e3e7c70fbe855d05afb5dd5d09695a1d
Patrick Weizhi Xu <weizhi.xu@zilliz.com>
2021-10-18 15:04:31 +0800

    update docker framework
    Signed-off-by: Patrick Weizhi Xu <weizhi.xu@zilliz.com>

--- python/run_framework.sh
+++ python/run_framework.sh
@@ -7,8 +7,12 @@
 ALGORITHM=bbann
 set -e
 pushd ../benchmark
+cp ../python/Dockerfile.bbann  install/
 cp ../python/bbann.py benchmark/algorithms/bbann.py
 rm -rf results/random-xs/*
-python3 run.py --definitions ../python/bbann-algo.yaml --nodocker --dataset random-xs --algorithm $ALGORITHM --force --rebuild
+
+python3 install.py --install bbann
+
+python3 run.py --definitions ../python/bbann-algo.yaml --dataset random-xs --algorithm $ALGORITHM # --force --rebuild
 python3 plot.py --definitions ../python/bbann-algo.yaml --dataset random-xs --recompute
 popd
