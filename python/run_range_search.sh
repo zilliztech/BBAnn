f42a476a52a4cd7bb5173405907093bf30bb8099
Patrick Weizhi Xu <weizhi.xu@zilliz.com>
2021-10-18 20:17:37 +0800

    fix range search script
    Signed-off-by: Patrick Weizhi Xu <weizhi.xu@zilliz.com>

--- python/run_range_search.sh
+++ python/run_range_search.sh
@@ -15,11 +15,13 @@ make clean
 make BBAnnLib2_s
 popd
 
-python3 install.py --algorithm bbann
-
 set -e
 pushd ../benchmark
+cp ../python/Dockerfile.bbann  install/
 cp ../python/bbann.py benchmark/algorithms/bbann.py
+
+python3 install.py --algorithm bbann
+
 # rm -rf results/random-range-xs/*
 python3 run.py --definitions ../python/bbann-algo.yaml --dataset random-range-xs --algorithm bbann # --force --rebuild
 python3 plot.py --definitions ../python/bbann-algo.yaml --dataset  random-range-xs --recompute
