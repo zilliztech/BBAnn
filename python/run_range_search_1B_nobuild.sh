0f0f9abb52bf7affbb373ceed4641a3f0f14373f
Ubuntu <zilliz@vm3.dxcw5p4q5zsu1fruyuok5a1kgg.xx.internal.cloudapp.net>
2021-10-19 14:12:45 +0000

    add ssh

---
+++ python/run_range_search_1B_nobuild.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# To build the pybind11 integration, run the following:
+# need to create symbolic link ../benchmark/data at certain SSD.
+# then,
+# python3 create_dataset.py --datset random-xs
+
+set -e
+pushd ../benchmark
+cp ../python/bbann.py benchmark/algorithms/bbann.py
+rm -rf results/ssnpp-1B*
+python3 run.py --definitions ../python/bbann-algo.yaml --nodocker --dataset ssnpp-1B --algorithm bbann --runs 3 # --force --rebuild
+python3 plot.py --definitions ../python/bbann-algo.yaml --dataset ssnpp-1B --recompute
+popd
