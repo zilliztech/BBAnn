1d2e947d1f31198ccdfb9d423c5d1fd2ae1fa0de
Patrick Weizhi Xu <cepheidfov@gmail.com>
2021-10-19 11:43:53 +0800

    delete legacy codes (#157)
    Signed-off-by: Patrick Weizhi Xu <weizhi.xu@zilliz.com>

--- playground/dataset-partition/run.sh
+++
@@ -1,9 +0,0 @@
-python3 analyze_partition.py --partition_type kmeans --dataset_name bigann10K --n_cluster 113 --data_set_path /mnt/Billion-Scale/BIGANN/base.10K.u8bin
-python3 analyze_partition.py --partition_type graph_partition --dataset_name bigann10K --n_cluster 113 --data_set_path /mnt/Billion-Scale/BIGANN/base.10K.u8bin
-
-python3 analyze_partition.py --partition_type kmeans --dataset_name bigann100K --n_cluster 1000 --data_set_path /mnt/Billion-Scale/BIGANN/base.100K.u8bin
-python3 analyze_partition.py --partition_type graph_partition --dataset_name bigann100K --n_cluster 1000 --data_set_path /mnt/Billion-Scale/BIGANN/base.100K.u8bin
-
-
-python3 analyze_partition.py --partition_type kmeans --dataset_name bigann1M --n_cluster 10000 --data_set_path /mnt/Billion-Scale/BIGANN/base.1M.u8bin
-python3 analyze_partition.py --partition_type graph_partition --dataset_name bigann1M --n_cluster 10000 --data_set_path /mnt/Billion-Scale/BIGANN/base.1M.u8bin
