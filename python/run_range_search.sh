0f2a3a1efb6db9e28929ae5ca719f476c93ae42c
Patrick Weizhi Xu <weizhi.xu@zilliz.com>
2021-10-19 19:51:57 +0800

    use radius 96237 for range search
    Signed-off-by: Patrick Weizhi Xu <weizhi.xu@zilliz.com>

--- python/run_range_search.sh
+++ python/run_range_search.sh
@@ -23,6 +23,6 @@ cp ../python/bbann.py benchmark/algorithms/bbann.py
 python3 install.py --algorithm bbann
 
 # rm -rf results/random-range-xs/*
-python3 run.py --definitions ../python/bbann-algo.yaml --dataset random-range-xs --algorithm bbann # --force --rebuild
-python3 plot.py --definitions ../python/bbann-algo.yaml --dataset  random-range-xs --recompute
+python3 run.py --definitions ../python/bbann-algo.yaml --dataset random-range-xs --algorithm bbann --count 96237 # --force --rebuild
+python3 plot.py --definitions ../python/bbann-algo.yaml --dataset  random-range-xs --recompute --count 96237
 popd
