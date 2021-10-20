b343b1a9b19dc18ef78f7a1d7ea3a907ee237fcb
Patrick Weizhi Xu <cepheidfov@gmail.com>
2021-10-21 03:38:58 +0800

    Fix docker file (#176)
    * add top-level dockerfile
    
    Signed-off-by: Patrick Weizhi Xu <weizhi.xu@zilliz.com>
    
    * add dockerignore file
    
    Signed-off-by: Patrick Weizhi Xu <weizhi.xu@zilliz.com>

--- python/run_range_search.sh
+++ python/run_range_search.sh
@@ -4,25 +4,29 @@
 # then,
 # python3 create_dataset.py --datset random-xs
 
-set -e
-mkdir -p ../build
-pushd ../build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-cd src/
-make TimeRecorder
-cd lib
-make clean
-make BBAnnLib2_s
+# set -e
+# mkdir -p ../build
+# pushd ../build
+# cmake .. -DCMAKE_BUILD_TYPE=Release
+# cd src/
+# make TimeRecorder
+# cd lib
+# make clean
+# make BBAnnLib2_s
+# popd
+
+pushd ../
+docker build --rm -f Dockerfile.local -t billion-scale-benchmark-bbann .
 popd
-
-
-sudo rm -rf build/
-sudo python3 setup.py install -f
 
 set -e
 pushd ../benchmark
+cp ../python/Dockerfile.bbann  install/
 cp ../python/bbann.py benchmark/algorithms/bbann.py
-rm -rf results/ssnpp-10M
-python3 run.py --definitions ../python/bbann-algo.yaml --nodocker --dataset ssnpp-10M --algorithm bbann --runs 1 # --force --rebuild
-python3 plot.py --definitions ../python/bbann-algo.yaml --dataset ssnpp-10M --recompute
+
+# python3 install.py --algorithm bbann
+
+# rm -rf results/random-range-xs/*
+python3 run.py --definitions ../python/bbann-algo.yaml --dataset random-range-xs --algorithm bbann # --force --rebuild
+python3 plot.py --definitions ../python/bbann-algo.yaml --dataset  random-range-xs --recompute
 popd
