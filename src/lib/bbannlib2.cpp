5d6e4aaa08f3036c8c154ad5dd014e03737aa046
Patrick Weizhi Xu <weizhi.xu@zilliz.com>
2021-10-22 03:39:37 +0800

    fix vec
    Signed-off-by: Patrick Weizhi Xu <weizhi.xu@zilliz.com>

--- src/lib/bbannlib2.cpp
+++ src/lib/bbannlib2.cpp
@@ -52,7 +52,6 @@ void search_bbann_queryonly(
   uint32_t cid, bid;
   const uint32_t vec_size = sizeof(DATAT) * dim;
   const uint32_t entry_size = vec_size + sizeof(uint32_t);
-  DATAT *vec;
 
   std::function<void(size_t, DISTT *, uint32_t *)> heap_heapify_func;
   std::function<bool(DISTT, DISTT)> cmp_func;
@@ -222,6 +221,7 @@ void search_bbann_queryonly(
         (num + io_wait_threads_num - 1) / io_wait_threads_num;
 
 #pragma omp parallel for
+    DATAT *vec;
     for (auto th = 0; th < io_wait_threads_num; th++) {
       auto begin_th = begin + wait_num_per_batch * th;
       auto end_th = std::min(int(begin_th + wait_num_per_batch), end);
