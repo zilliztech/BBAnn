edccf2b915d6f467a12d1fc90242575df09f7fe7
Patrick Weizhi Xu <cepheidfov@gmail.com>
2021-10-22 15:45:24 +0800

    update range search scripts (#190)
    Signed-off-by: Patrick Weizhi Xu <weizhi.xu@zilliz.com>

--- python/run_range_search_nobuild.sh
+++ python/run_range_search_nobuild.sh
@@ -8,6 +8,6 @@ set -e
 pushd ../benchmark
 cp ../python/bbann.py benchmark/algorithms/bbann.py
 rm -rf results/ssnpp-10M
-python3 run.py --definitions ../python/bbann-algo.yaml --nodocker --dataset ssnpp-10M --algorithm bbann --runs 1 # --force --rebuild
-python3 plot.py --definitions ../python/bbann-algo.yaml --dataset ssnpp-10M --recompute
+python3 run.py --definitions ../python/bbann-algo.yaml --nodocker --dataset ssnpp-10M --algorithm bbann --runs 1 --count 96237 # --force --rebuild
+python3 plot.py --definitions ../python/bbann-algo.yaml --dataset ssnpp-10M --recompute --count 96237
 popd
