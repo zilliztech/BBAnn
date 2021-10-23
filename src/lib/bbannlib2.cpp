eaec71c8fccbd37229602e8a61177d479d63ee16
xiaofan-luan <xiaofan.luan@zilliz.com>
2021-10-23 22:09:47 +0800

    Fix

--- src/lib/bbannlib2.cpp
+++ src/lib/bbannlib2.cpp
@@ -239,7 +239,7 @@ auto fio_way = [&](io_context_t aio_ctx, std::vector<char *> &bufs, int begin, i
 
 
     std::atomic<bool> stop (false);
-    auto computer = [&](std::vector<std::vector<char *>>& taskQueues, int nqStart, int nqEnd) {
+    auto computer = [&](int nqStart, int nqEnd) {
         std::cout<<"computer start, start " << nqStart << "end " << nqEnd << std::endl;
         const uint32_t vec_size = sizeof(DATAT) * dim;
         const uint32_t entry_size = vec_size + sizeof(uint32_t);
